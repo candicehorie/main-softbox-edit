e2cdc38f09b005857add3cc01aa9a0364fae665e
candicehorie <candicehorie@gmail.com>
2021-02-21 15:41:18 -0300

    Apaguei o documento

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,45 @@
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    scripts: []
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    scripts: [],
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
   css: [
-    { src: 'bootstrap/dist/css/bootstrap.min.css', lang: 'css' },
-    { src: '@fortawesome/fontawesome-free/css/all.min.css', lang: 'css' },
-    { src: 'animate.css/animate.min.css', lang: 'css' },
-    { src: '~assets/app/app.css', lang: 'css' },
-    { src: 'animate.css/animate.min.css', lang: 'css' },
-    { src: 'owl.carousel/dist/assets/owl.carousel.css', lang: 'css' },
-    { src: 'owl.carousel/dist/assets/owl.theme.default.css', lang: 'css' }
+    { src: "bootstrap/dist/css/bootstrap.min.css", lang: "css" },
+    { src: "@fortawesome/fontawesome-free/css/all.min.css", lang: "css" },
+    { src: "animate.css/animate.min.css", lang: "css" },
+    { src: "~assets/app/app.css", lang: "css" },
+    { src: "animate.css/animate.min.css", lang: "css" },
+    { src: "owl.carousel/dist/assets/owl.carousel.css", lang: "css" },
+    { src: "owl.carousel/dist/assets/owl.theme.default.css", lang: "css" },
   ],
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~plugins/sofbox.js'],
+  plugins: ["~plugins/sofbox.js"],
 
   router: {
-    base: '/sofbox-vue/'
+    base: "/",
   },
 
   /*
@@ -54,9 +54,9 @@ export default {
    */
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    "bootstrap-vue/nuxt",
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
   ],
   /*
    ** Build configuration
@@ -66,23 +66,31 @@ export default {
      ** You can extend webpack config here
      */
     extractCSS: {
-      allChunks: true
+      allChunks: true,
     },
     splitChunks: {
-      layouts: true
+      layouts: true,
     },
-    vendor: ['jquery', 'bootstrap'],
-    extend (config, ctx) {},
-    babel: { compact: true }
+    vendor: ["jquery", "bootstrap"],
+    extend(config, ctx) {},
+    babel: { compact: true },
   },
   server: {
-    port: 5700 // default: 3000
+    port: 5700, // default: 3000
   },
   vue: {
     config: {
       ignoredElements: [
-        'rs-module-wrap', 'rs-module', 'rs-bgvideo', 'rs-slides', 'rs-layer', 'rs-static-layers', 'rs-progress', 'rs-slide', 'rs-3'
-      ]
-    }
-  }
-}
+        "rs-module-wrap",
+        "rs-module",
+        "rs-bgvideo",
+        "rs-slides",
+        "rs-layer",
+        "rs-static-layers",
+        "rs-progress",
+        "rs-slide",
+        "rs-3",
+      ],
+    },
+  },
+};
